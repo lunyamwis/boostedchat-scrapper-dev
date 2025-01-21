752c432daff28dac7535fcd655f19c539714e887
lunyamwis <lutherlunyamwi@gmail.com>
2025-01-21 14:26:56 +0300

    repairing package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lunyamwi',
-    version='1.0.10',
+    version='1.0.11',
     author='Martin Luther Bironga',
     description='Lunyamwi is a data science library that assists one in data generation, pipeline setup, model selection and setup.',
     long_description=open('README.md').read(),
