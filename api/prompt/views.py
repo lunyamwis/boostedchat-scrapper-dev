b4aab7fa505c9e98213fc68789a78226e3c9abb3
lunyamwis <lutherlunyamwi@gmail.com>
2025-01-29 16:46:27 +0300

    test

--- api/prompt/views.py
+++ api/prompt/views.py
@@ -919,7 +919,7 @@ class agentSetup(APIView):
                 # process=Process.sequential,
                 verbose=True,
                 memory=True,
-                output_log_file=logging_filename
+                # output_log_file=logging_filename
             )
             
             
@@ -933,56 +933,56 @@ class agentSetup(APIView):
             # if isinstance(result, dict):
                 # kickstart new workflow
             # send_logs.delay(data,result.json_dict)
-            with wandb.init(
-                    project="boostedchat",  # replace with your WandB project name
-                    entity="lutherlunyamwi",       # replace with your WandB username or team
-                    name=f"crewai_run_{data.get('department')}",  # custom name for each run
-                    config=data,           # optionally log the request data as run config
-                    settings=wandb.Settings(
-                        _service_wait=1200,  # Increase service wait time to 600 seconds
-                        init_timeout=1200     # Increase initialization timeout to 600 seconds
-                    )
+            # with wandb.init(
+            #         project="boostedchat",  # replace with your WandB project name
+            #         entity="lutherlunyamwi",       # replace with your WandB username or team
+            #         name=f"crewai_run_{data.get('department')}",  # custom name for each run
+            #         config=data,           # optionally log the request data as run config
+            #         settings=wandb.Settings(
+            #             _service_wait=1200,  # Increase service wait time to 600 seconds
+            #             init_timeout=1200     # Increase initialization timeout to 600 seconds
+            #         )
                     
-                ) as run:
-                wandb_handler = WandbLoggingHandler()
-                wandb_handler.setLevel(logging.INFO)
-                wandb_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+            #     ) as run:
+            #     wandb_handler = WandbLoggingHandler()
+            #     wandb_handler.setLevel(logging.INFO)
+            #     wandb_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 
-                langchain_logger = logging.getLogger("langchain")
-                langchain_logger.addHandler(wandb_handler)
-                langchain_logger.setLevel(logging.INFO)
+            #     langchain_logger = logging.getLogger("langchain")
+            #     langchain_logger.addHandler(wandb_handler)
+            #     langchain_logger.setLevel(logging.INFO)
                 
-                if opensource:
-                    try:
-                        wandb.log({"result":result.raw})
-                    except Exception as err:
-                        print(err)
-                else:
-                    try:
-                        wandb.log({"result": result.json_dict})  # log the final result
-                    except Exception as e:
-                        print(e)
+            #     if opensource:
+            #         try:
+            #             wandb.log({"result":result.raw})
+            #         except Exception as err:
+            #             print(err)
+            #     else:
+            #         try:
+            #             wandb.log({"result": result.json_dict})  # log the final result
+            #         except Exception as e:
+            #             print(e)
                 # Optionally, log additional information about agents and tasks
-                with wandb.init(
-                    project="boostedchat",  # replace with your WandB project name
-                    entity="lutherlunyamwi",       # replace with your WandB username or team
-                    name=f"crewai_run_{data.get('department')}",  # custom name for each run
-                    config=data,           # optionally log the request data as run config
-                    settings=wandb.Settings(
-                        _service_wait=1200,  # Increase service wait time to 600 seconds
-                        init_timeout=1200     # Increase initialization timeout to 600 seconds
-                    )
+                # with wandb.init(
+                #     project="boostedchat",  # replace with your WandB project name
+                #     entity="lutherlunyamwi",       # replace with your WandB username or team
+                #     name=f"crewai_run_{data.get('department')}",  # custom name for each run
+                #     config=data,           # optionally log the request data as run config
+                #     settings=wandb.Settings(
+                #         _service_wait=1200,  # Increase service wait time to 600 seconds
+                #         init_timeout=1200     # Increase initialization timeout to 600 seconds
+                #     )
                     
-                ) as run:
-                    wandb.log({
-                        "agents": [{"role": agent.role, "goal": agent.goal, "tools": str(agent.tools)} for agent in agents],
-                        "tasks": [{"description": task.description, "expected_output": task.expected_output} for task in tasks]
-                    })
+                # ) as run:
+                #     wandb.log({
+                #         "agents": [{"role": agent.role, "goal": agent.goal, "tools": str(agent.tools)} for agent in agents],
+                #         "tasks": [{"description": task.description, "expected_output": task.expected_output} for task in tasks]
+                #     })
 
-                    # End wandb run
-                    time.sleep(2)
-                    self.log_scrapping_logs(logging_filename)
-                    wandb.finish()
+                #     # End wandb run
+                #     time.sleep(2)
+                #     self.log_scrapping_logs(logging_filename)
+                #     wandb.finish()
             # import pdb;pdb.set_trace()
             if opensource:
                 # import pdb;pdb.set_trace()
