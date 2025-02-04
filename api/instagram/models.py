09ec542022b218f51bb3317cc7fa4cff6e10acf8
dennis mokaya <anyoksdenn@gmail.com>
2025-02-04 09:08:59 +0300

    download link update

--- api/instagram/models.py
+++ api/instagram/models.py
@@ -269,7 +269,8 @@ class Media(BaseModel):
     timestamp = models.DateTimeField(null=True,blank=True)
     item_id = models.CharField(max_length=255,null=True,blank=True)
     item_type = models.CharField(max_length=255,null=True,blank=True)
-    download_url = models.URLField(null=True,blank=True)
+    download_url = models.URLField(max_length=2048,null=True,blank=True)
+    
     
 
     def __str__(self) -> str:
