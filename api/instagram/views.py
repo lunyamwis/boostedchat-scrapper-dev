1c2de31b186c8732af7a89b22d658718455e75f4
lunyamwis <lutherlunyamwi@gmail.com>
2025-02-24 08:46:05 +0300

    unpausing dag

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -543,7 +543,25 @@ class TriggerRun(View):
         
         workflow = WorkflowModel.objects.get(id=kwargs['pk'])
         dag_id = workflow.dagmodel_set.latest('created_at').dag_id
-
+        try:
+            airflowcreds = AirflowCreds.objects.latest('created_at')
+            headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+            dag_update_data = {
+                "is_paused": False
+            }
+            resp = requests.patch(f"{airflowcreds.airflow_base_url}/api/v1/dags/{dag_id}", 
+                                    data=json.dumps(dag_update_data),
+                                    auth=HTTPBasicAuth(airflowcreds.username, airflowcreds.password),
+                                    headers=headers)
+            if resp.status_code in [200,201]:
+                messages.success(request, "DAG unpaused successfully")
+            else:
+                messages.error(request, f"Failed to unpause DAG: {resp.text}")
+        except Exception as err:
+            messages.error(request, f"Failed to unpause DAG: {str(err)}")
         # Trigger the DAG run
         try:
             airflowcreds = AirflowCreds.objects.latest('created_at')
