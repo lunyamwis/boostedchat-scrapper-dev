fcd16d9fb9b1e1e0a777dd3593d908354aa268e5
lunyamwis <lutherlunyamwi@gmail.com>
2025-03-27 12:54:54 +0300

    testing

--- boostedchatScrapper/spiders/instagram.py
+++ boostedchatScrapper/spiders/instagram.py
@@ -572,7 +572,7 @@ class InstagramSpider:
                 time.sleep(random.randint(delay_before_requests,delay_before_requests+step))
                 try:
 
-                    info_dict_ = client.user_info_by_username(user.username).model_dump_json()
+                    info_dict_ = client.user_info_by_username_v1(user.username).model_dump_json()
                     info_dict = json.loads(info_dict_)
                     if user.item_id:
                         info_dict.update({"media_id":user.item_id})
@@ -580,11 +580,11 @@ class InstagramSpider:
                         # comment = self.generate_comment(user_medias[0],user.username)
                         # info_dict.update({"media_comment":comment})
                         if not info_dict.get("is_private"):
-                            user_medias = client.user_medias(info_dict.get("pk"),amount=1)
+                            user_medias = client.user_medias_v1(info_dict.get("pk"),amount=1)
                             media_res = []
                             for user_media in user_medias:
                                 if user_media:
-                                    media_info_ = client.media_info(user_media.id)
+                                    media_info_ = client.media_info_v1(user_media.id)
                                     media_res.append({
                                         "media_id":media_info_.id,
                                         "media_url":media_info_.thumbnail_url.unicode_string(),
