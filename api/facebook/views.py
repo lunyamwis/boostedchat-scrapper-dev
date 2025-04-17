4d0fd10d4dc6591fb4949273489f64f5de16406a
lunyamwis <lutherlunyamwi@gmail.com>
2025-04-17 19:43:42 +0300

    test

--- api/facebook/views.py
+++ api/facebook/views.py
@@ -29,7 +29,8 @@ def webhook(request):
         data = json.loads(request.body.decode('utf-8'))
 
         if data.get('object') == 'page':
-            break
+            # raise Exception("Webhook received a page object")
+            # continue
             # send_message("9581548405296563","Been hustling hard")
             
             # for entry in data.get('entry', []):
@@ -49,7 +50,7 @@ def webhook(request):
             #                 # Send reply
             #                 # send_message(sender_id, reply)
 
-            # return HttpResponse('EVENT_RECEIVED')
+            return HttpResponse('ONE_EVENT_RECEIVED')
         else:
             return HttpResponse(status=404)
 
