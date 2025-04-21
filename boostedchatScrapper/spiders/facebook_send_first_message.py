0961b3302b1866a5328090243eaf38490cc02f6a
lunyamwis <lutherlunyamwi@gmail.com>
2025-04-21 15:45:39 +0300

    testing

--- boostedchatScrapper/spiders/facebook_send_first_message.py
+++ boostedchatScrapper/spiders/facebook_send_first_message.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
@@ -36,7 +37,7 @@ def convert_cookies(input_cookies: List[Dict[str, Any]]) -> List[Dict[str, Any]]
 
 
 
-def send_first_message(cookies_, user_id):
+def send_first_message(cookies_, user_id,username=None):
     """Define a main entry point."""
 
     # Handle input - Replace with your input method (e.g., command line arguments, config file)
@@ -100,24 +101,79 @@ def send_first_message(cookies_, user_id):
 
         
         time.sleep(5)
-        get_url(driver, f"https://www.facebook.com/messages/t/{user_id}")
+        get_url(driver, f"https://www.facebook.com/messages/new")
+
+
         time.sleep(5)
+
         try:
-            continue_ = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div")
-            continue_.click()
+            # Wait for the message box to be present
+            input_box = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[1]/input"))
+            )
+            input_box.click()
+            input_box.send_keys(username)
+            
             time.sleep(5)
+            logging.info(f"Successfully inserted contact!")
         except Exception as e:
-            logging.warning(f"Failed to click continue button: {e}")
+            logging.warning(f"Failed to insert contact: {e}")
         
         try:
-            message_box = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div[4]/div[2]/div/div[1]/div[1]")
-            message_box.send_keys("go ahead!")
-            message_box.send_keys(Keys.ENTER)
-
-            # Save results to Apify dataset
-            logging.info(f'Successfully reached out!')
+            # Wait until the element is clickable (up to 10 seconds)
+            element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[2]/div/div/div[1]/div[1]/ul/li[1]/ul/div[1]/li'))
+            )
+            element.click()
+            time.sleep(5)
+            # Perform the click action
+            logging.info("Element clicked successfully!")
+            # mainframe = WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div"))  
+            # )
+            # mainframe.click()
+            actions = ActionChains(driver)
+            actions.send_keys(Keys.ENTER).perform()
+            logging.info("Mainframe clicked successfully!")
         except Exception as e:
-            logging.warning(f"Unsuccessful in sending message: {e}")
+            logging.warning(f"Error: {e}")
+        
+        try:
+            # Wait for the message box to be present
+            message_textbox = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[2]/div[2]/div/div/div/div/div[4]/div[2]/div/div[1]/div[1]"))
+            )
+            # message_box = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div[4]/div[2]/div/div[1]/div[1]")
+            message_textbox.click()
+            message_textbox.send_keys("hello friend")
+            message_textbox.send_keys(Keys.ENTER)
+            logging.info(f"Successfully sent message!")
+            time.sleep(5)
+        except Exception as e:
+            logging.warning(f"Failed to find message box: {e}")
+            
+        
+        # try:
+        #     continue_ = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div")
+        #     continue_.click()
+        #     time.sleep(5)
+        # except Exception as e:
+        #     logging.warning(f"Failed to click continue button: {e}")
+        
+        # try:
+        #     time.sleep(5)
+        #     message_box = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div[4]/div[2]/div/div[1]/div[1]")
+        #     message_box.send_keys("go ahead!")
+        #     message_box.send_keys(Keys.ENTER)
+
+        #     # Save results to Apify dataset
+        #     logging.info(f'Successfully reached out!')
+        # except Exception as e:
+        #     logging.warning(f"Unsuccessful in sending message: {e}")
+
+        
+        
+        
         
 
     except Exception as e:
