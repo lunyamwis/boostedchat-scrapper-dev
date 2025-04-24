30cb2f11ef575e624a933e191e5125aa8516d98a
lunyamwis <lutherlunyamwi@gmail.com>
2025-04-24 12:22:25 +0300

    testing

--- api/facebook/forms.py
+++ api/facebook/forms.py
@@ -9,4 +9,4 @@ class ScrapFacebookGroupForm(forms.Form):
 class SendFirstMessageForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
     cookies = forms.CharField(label="Cookies", widget=forms.Textarea)
-    message = forms.CharField(label="Message", max_length=1024)
+    message = forms.CharField(label="Message", widget=forms.Textarea)
