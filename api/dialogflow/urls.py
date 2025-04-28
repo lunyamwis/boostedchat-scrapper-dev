1d239c17f2e2d269651bc73af696d8f3a4d937de
lunyamwis <lutherlunyamwi@gmail.com>
2025-04-28 13:45:00 +0300

    adding

--- api/dialogflow/urls.py
+++ api/dialogflow/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from . import views
-from api.instagram.views import initialize_db
 
 urlpatterns = [
     path("fallbackWebhook/", views.FallbackWebhook.as_view()),
-    path("init/", initialize_db, name="initialize_db")
 ]
