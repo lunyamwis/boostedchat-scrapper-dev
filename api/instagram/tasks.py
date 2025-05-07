1a570976a471caaac31ec0558365387486826787
lunyamwis <lutherlunyamwi@gmail.com>
2025-05-07 11:55:25 +0300

    testing first

--- api/instagram/tasks.py
+++ api/instagram/tasks.py
@@ -845,18 +845,33 @@ def reschedule():
 @shared_task()
 @schema_context(os.getenv("SCHEMA_NAME"))
 def prequalify_task():
+
+    # yesterday = timezone.now().date() - timezone.timedelta(days=1)
+    # yesterday_start = timezone.make_aware(timezone.datetime.combine(yesterday, timezone.datetime.min.time()))
+    # unwanted_usernames = UnwantedAccount.objects.values_list('username', flat=True)
+
+    # # Filter accounts that are qualified and created from yesterday onwards, and exclude accounts that are not wanted
+    # accounts = Account.objects.filter(
+    #     Q(qualified=False) & Q(created_at__gte=yesterday_start)
+    # ).exclude(
+    #     status__name="sent_compliment"
+    # ).exclude(
+    #     igname__in=unwanted_usernames
+    # )
     yesterday = timezone.now().date() - timezone.timedelta(days=1)
+    tomorrow = timezone.now().date() + timezone.timedelta(days=1)
     yesterday_start = timezone.make_aware(timezone.datetime.combine(yesterday, timezone.datetime.min.time()))
     unwanted_usernames = UnwantedAccount.objects.values_list('username', flat=True)
 
     # Filter accounts that are qualified and created from yesterday onwards, and exclude accounts that are not wanted
     accounts = Account.objects.filter(
-        Q(qualified=False) & Q(created_at__gte=yesterday_start)
+        Q(qualified=True) & Q(created_at__gte=yesterday_start) & Q(created_at__lte=tomorrow)
     ).exclude(
         status__name="sent_compliment"
     ).exclude(
         igname__in=unwanted_usernames
     )
+    
     if accounts.exists():
         
         for account in accounts:
