5e5e3870470e7a8a7e714cd5991679eb658c910b
dennis mokaya <anyoksdenn@gmail.com>
2025-05-08 09:00:51 +0300

    serialize thread_id

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -427,7 +427,8 @@ class AccountViewSet(viewsets.ModelViewSet):
                 .order_by('-sent_on')
                 .values('sent_by')[:1]
             ),
-            latest_message_at=Coalesce('last_message_sent_at', 'last_message_at', Value(datetime.min))
+            latest_message_at=Coalesce('last_message_sent_at', 'last_message_at', Value(datetime.min)),
+            thread_id=Subquery(Thread.objects.filter(account=OuterRef('pk')).values('thread_id')[:1])
         ).order_by('-created_at')#order_by('-latest_message_at')
 
         # Filters from request
