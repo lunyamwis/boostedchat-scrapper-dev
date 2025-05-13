2d4650b7cf436a656c2c06a51956d8a006d714ae
dennis mokaya <anyoksdenn@gmail.com>
2025-05-13 13:07:17 +0300

    add relevant and outsourced info

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -812,22 +812,46 @@ class AccountViewSet(viewsets.ModelViewSet):
         
         # Get or create account based on title
         try:
-            account,created = Account.objects.get_or_create(igname=igname.strip(),  
-                                                            qualified=True,
-                                                            outreach_success=False,
-                                                            outreach_time=outreach_date,
-                                                            responded_date=responded_date,
-                                                            call_scheduled_date=call_scheduled_date,
-                                                            closing_date=closing_date,
-                                                            won_date=won_date,
-                                                            success_story_date=success_story_date,
-                                                            lost_date=lost_date,
-                                                            full_name=full_name)
+            print("****** creating account ********")
+            account =  Account.objects.filter(igname=igname.strip()).first()
+            
+            if account is None:
+                account,created = Account.objects.get_or_create(igname=igname.strip(),  
+                                                                qualified=True,
+                                                                outreach_success=False,
+                                                                outreach_time=outreach_date,
+                                                                responded_date=responded_date,
+                                                                call_scheduled_date=call_scheduled_date,
+                                                                closing_date=closing_date,
+                                                                won_date=won_date,
+                                                                success_story_date=success_story_date,
+                                                                relevant_information={},
+                                                                lost_date=lost_date,
+                                                                full_name=full_name)
+            else:
+                account.qualified = True
+                account.outreach_success = False
+                account.outreach_time = outreach_date
+                account.responded_date = responded_date
+                account.call_scheduled_date = call_scheduled_date
+                account.closing_date = closing_date
+                account.won_date = won_date
+                account.success_story_date = success_story_date
+                account.lost_date = lost_date
+                account.full_name = full_name
+                if account.relevant_information is None:
+                    account.relevant_information = {}
+                    
+                account.save()
+                
+                
             if outreach_date:
                 account.outreach_success = True
                 account.created_at = outreach_date
                 account.status = StatusCheck.objects.get(name="sent_compliment")
                 account.save()
+                
+                
           
             serializer = AccountSerializer(account)
             assign_salesrep(account)    
