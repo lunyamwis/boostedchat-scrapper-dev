739c2da6caf2ceef13f1b10a14d63cf0d18a5609
lunyamwis <lutherlunyamwi@gmail.com>
2025-05-21 11:58:04 +0300

    test

--- api/instagram/tasks.py
+++ api/instagram/tasks.py
@@ -475,13 +475,13 @@ def send_first_compliment(username, message, repeat=True):
                     thread_obj.account = account
                     thread_obj.last_message_content = first_message
                     thread_obj.unread_message_count = 0
-                    thread_obj.last_message_at = datetime.datetime.fromtimestamp(int(returned_data['timestamp'])/1000000) # use UTC
+                    thread_obj.last_message_at = datetime.fromtimestamp(int(returned_data['timestamp'])/1000000) # use UTC
                     thread_obj.save()
 
                     message = Message()
                     message.content = first_message
                     message.sent_by = "Robot"
-                    message.sent_on = datetime.datetime.fromtimestamp(int(returned_data["timestamp"]) / 1000000)
+                    message.sent_on = datetime.fromtimestamp(int(returned_data["timestamp"]) / 1000000)
                     message.thread = thread_obj
                     message.save()
                     print("message created then saved")
@@ -493,13 +493,13 @@ def send_first_compliment(username, message, repeat=True):
                         thread_obj.account = account
                         thread_obj.last_message_content = first_message
                         thread_obj.unread_message_count = 0
-                        thread_obj.last_message_at = datetime.datetime.fromtimestamp(int(returned_data['timestamp'])/1000000) # use UTC
+                        thread_obj.last_message_at = datetime.fromtimestamp(int(returned_data['timestamp'])/1000000) # use UTC
                         thread_obj.save()
 
                         message = Message()
                         message.content = first_message
                         message.sent_by = "Robot"
-                        message.sent_on = datetime.datetime.fromtimestamp(int(returned_data["timestamp"]) / 1000000)
+                        message.sent_on = datetime.fromtimestamp(int(returned_data["timestamp"]) / 1000000)
                         message.thread = thread_obj
                         message.save()
                         print("message is saved")
