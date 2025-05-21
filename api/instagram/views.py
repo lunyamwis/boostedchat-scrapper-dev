d5611429064e773563fb620973d4dfc1578855f5
lunyamwis <lutherlunyamwi@gmail.com>
2025-05-21 10:00:34 +0300

    testing

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -2923,8 +2923,8 @@ class DMViewset(viewsets.ModelViewSet):
                 )
                 print(result)
                 return Response({
-                    # "generated_comment": gpt_resp,
-                    "generated_comment": "cool stuff!",
+                    "generated_comment": gpt_resp,
+                    # "generated_comment": "cool stuff!",
                     "text": query,
                     "success": True,
                     "username": thread.account.igname,
