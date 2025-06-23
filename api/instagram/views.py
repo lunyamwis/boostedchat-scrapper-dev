1431330bba207b3362c2f6836bc5850c1db674b9
lunyamwis <lutherlunyamwi@gmail.com>
2025-06-23 10:25:50 +0300

    rectifying string issue

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -472,7 +472,7 @@ class GetMediaLikers(APIView):
                 # Fetch the likers of the media
                 influencer = random.choice(influencers_list)
                 logging.warning(f"influencer chosen ---->{influencer}")
-                latest_influencer_media = cl.user_medias(user_id=cl.user_by_username_v1(username=influencer.get("pk")))[0]
+                latest_influencer_media = cl.user_medias(user_id=cl.user_by_username_v1(username=influencer).get("pk"))[0]
                 # media_id = cl.media_pk_from_url_v1(link)
                 likers = cl.media_likers_v2(latest_influencer_media.get("pk"))
                 for liker in likers['users']:
