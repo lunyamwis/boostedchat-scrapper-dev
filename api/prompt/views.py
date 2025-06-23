c47f9ec06de848fb550fd4b6d1f800ecc2c22b73
lunyamwis <lutherlunyamwi@gmail.com>
2025-06-23 11:53:58 +0300

    working on it

--- api/prompt/views.py
+++ api/prompt/views.py
@@ -948,7 +948,7 @@ class PrequalifyingWorkflow(Flow):
             "prequalified":True,
             "desired_location":desired_location, 
             "desired_category": desired_category, 
-            "desired_visibility":not self.inputs['outsourced_info']['is_private'], 
+            # "desired_visibility":not self.inputs['outsourced_info']['is_private'], 
             "desired_activity": desired_activity, 
             "desired_provider": desired_provider, 
             "desired_size": desired_size
@@ -958,7 +958,7 @@ class PrequalifyingWorkflow(Flow):
             "prequalified":False,
             "desired_location":desired_location, 
             "desired_category": desired_category, 
-            "desired_visibility":not self.inputs['outsourced_info']['is_private'], 
+            # "desired_visibility":not self.inputs['outsourced_info']['is_private'], 
             "desired_activity": desired_activity, 
             "desired_provider": desired_provider, 
             "desired_size": desired_size
