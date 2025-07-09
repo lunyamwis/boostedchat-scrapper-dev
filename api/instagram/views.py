bb22366212650b8de9032011f6ad95c818a38448
dennis mokaya <anyoksdenn@gmail.com>
2025-07-09 10:12:52 +0300

    compute experiment results

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -3641,7 +3641,7 @@ class ExperimentViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as error:
             return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
-        
+          
     @schema_context(os.getenv('SCHEMA_NAME'))
     @action(detail=True, methods=['post'])
     def duplicate(self, request, pk=None):
