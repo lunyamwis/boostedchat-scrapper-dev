ef4c1403efbe604311063667a07cea27860fde2d
lunyamwis <88100401+lunyamwis@users.noreply.github.com>
2025-07-10 10:54:29 +0300

    monitoring state

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -505,7 +505,8 @@ class GetMediaLikers(APIView):
                 latest_influencer_media = cl.user_medias(user_id=cl.user_by_username_v1(username=influencer).get("pk"),count=1)[0]
                 # media_id = cl.media_pk_from_url_v1(link)
                 likers = cl.media_likers_v2(latest_influencer_media.get("pk"))
-                for liker in likers['users']:
+                for i,liker in enumerate(likers['users']):
+                    logging.warning(f"state: {i} out of {len(likers['users'])}")
                     check_user_exists = cl.user_by_username_v1(liker['username'])
                     if 'exc_type' in check_user_exists.keys():
                         continue
