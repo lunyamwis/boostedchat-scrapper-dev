e1ab454cb7a4dd114315ca18300e65e20769633a
lunyamwis <lutherlunyamwi@gmail.com>
2025-09-08 20:42:20 +0300

    changed setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="lunyamwi",
-    version="1.0.18",
+    version="1.0.19",
     author="Martin Luther Bironga",
     description="Lunyamwi is a social media management and automation tool.",
     long_description=long_description,
