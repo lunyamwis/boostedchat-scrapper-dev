dcc40230e5166e31ec6eb7d88dc105fd151edc72
lunyamwis <lutherlunyamwi@gmail.com>
2025-09-10 17:44:30 +0300

    allowing authentication

--- billing/views.py
+++ billing/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 # Create your views here.
 
 class PaystackWebhookView(APIView):
+    permission_classes = [AllowAny]  # Allow any user (authenticated or not) to access this view
     def post(self, request, *args, **kwargs):
         # Handle the webhook payload
         payload = request.data
