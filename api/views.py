419d3d373d3acdc3a7eeacc3621a48220f82a44d
lunyamwis <lutherlunyamwi@gmail.com>
2025-09-19 16:40:04 +0300

    reverts

--- api/views.py
+++ api/views.py
@@ -189,12 +189,12 @@ class TenantOAuth2CallbackView(View):
             tenant_name = user.client_set.last().name
             # user = authenticate(request, username=user.username, password=user.password)
             # if user:
-            # try:
-            #     login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            # except Exception as e:
-            #     logger.warning("Error logging in user: %s", e)
-            # return redirect(f"https://{tenant_name}.lunyamwi.org/workflow/")
-            return redirect("tenant_redirect")
+            try:
+                login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            except Exception as e:
+                logger.warning("Error logging in user: %s", e)
+            return redirect(f"https://{tenant_name}.lunyamwi.org/workflow/")
+            # return redirect("tenant_redirect")
             # return redirect("/")
 
 
