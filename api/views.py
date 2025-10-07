78184c066e9b8704fe29d8a75ad6a6da7cad270c
lunyamwis <lutherlunyamwi@gmail.com>
2025-10-07 21:38:51 +0300

    debugging

--- api/views.py
+++ api/views.py
@@ -216,26 +216,26 @@ class TenantOAuth2CallbackView(View):
                     
                 except RequestException as e:
                     print("Failed to fetch user info: %s", e)
-                
+                user = None
                 try:
                     user = User.objects.get(email=email)
-                    try:
-                        # login(request, user)
-                        payload = {
-                            "provider": "GOOGLE_OAUTH",
-                            "refresh_token": tokens.get("refresh_token"),
-                            "access_token": tokens.get("access_token")
-                        }
+                    # try:
+                    #     # login(request, user)
+                    #     payload = {
+                    #         "provider": "GOOGLE_OAUTH",
+                    #         "refresh_token": tokens.get("refresh_token"),
+                    #         "access_token": tokens.get("access_token")
+                    #     }
                     
-                        result = make_lunyamwi_gmail_request("POST", "/accounts", data=payload)
-                        if result.status_code != 200:
-                            logger.warning("Failed to create Gmail account in Lunyamwi: %s", result.text)
-                        else:
-                            logger.info("Successfully created Gmail account in Lunyamwi")
-                            user.gmail_account_id = result.json().get("account_id")
-                            user.save()
-                    except Exception as e:
-                        logger.warning("Error creating Gmail account in Lunyamwi: %s", e)
+                    #     result = make_lunyamwi_gmail_request("POST", "/accounts", data=payload)
+                    #     if result.status_code != 200:
+                    #         logger.warning("Failed to create Gmail account in Lunyamwi: %s", result.text)
+                    #     else:
+                    #         logger.info("Successfully created Gmail account in Lunyamwi")
+                    #         user.gmail_account_id = result.json().get("account_id")
+                    #         user.save()
+                    # except Exception as e:
+                    #     logger.warning("Error creating Gmail account in Lunyamwi: %s", e)
 
                     token_exists = Token.objects.filter(user=user)
                     if token_exists.exists():
